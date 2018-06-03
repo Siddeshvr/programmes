54732cb8b5bec32922291fdfa511c93199350445
siddesh <siddeshvr1999@gmail.com>
2018-06-03 19:37:33 +0530

    Gene transform

--- genetic2.py
+++ genetic2.py
@@ -1,7 +1,7 @@
 
-import random
+import random            #Import random module...
 
-class person:
+class Genetic:
 
 	fit = []
 	matingpool = []
@@ -25,77 +25,90 @@ class person:
 			population[i].append(self.phenotype)
 
 	def fitness(self):	
+		self.fit = []
 		for i in population:
 			score = 0
-			for j in range (0,7):
+			for j in range (0,len(self.target)):
 				if population[i][0][j] == self.target[j]:
 					score = score+1
-			self.fit.append(score) 
-		#print(self.fit)
+			self.fit.append(score)
 
-	'''def fitness(self):
-		distance = 0
-		for i in population:
-			distance = self.target - population[i][0]
-			v = (1/distance)
-			x = int(v*100)
-			self.fit.append(x)
-		print(self.fit)      '''
 
-	def prepare_pool(self):
+	def prepare_pool(self):                          #Preparing mating pool...
+		self.matingpool = []
 		k = 0
 		for i in population:
 			for j in range (0,self.fit[k]):
 				self.matingpool.append(population[i][0])
 			k = k+1
-		#print(self.matingpool)
+
 
 	def parent_selection(self):
-		for i in range(0,10):
-			a = random.choice(self.matingpool)
-			b = random.choice(self.matingpool)
-			if a != b:
-				self.parent.append(a)
-				self.parent.append(b)
-				break
+		self.parent = []
+		if self.matingpool != []:    # if matingpool not empty...
+			for i in range(0,10):
+				a = random.choice(self.matingpool)
+				b = random.choice(self.matingpool)
+				if a != b:
+					self.parent.append(a)
+					self.parent.append(b)
+					break
+
 
 	def cross_over(self):
-		mid = int(7/2)
-		for i in range(0,mid+1):
-			self.child.append(self.parent[0][i])
-		for i in range(mid+1,7):
-			self.child.append(self.parent[1][i])
-		return self.child
+		self.child = []
+		mid = int(len(self.target)/2)
+		if self.parent != []:
+			for i in range(0,mid+1):
+				self.child.append(self.parent[0][i])
+			for i in range(mid+1,len(self.target)):
+				self.child.append(self.parent[1][i])
+			return self.child
+		else:
+			return self.child
 
 	def mutation(self):
 		c = ''.join(self.child)
 		fetch = [1,2,3,4,5,6,7,8,9,0]
-		for i in range(0,7):
+		for i in range(0,len(self.target)):
 			if random.choice(fetch) < self.mutation_rate:
-				c = c[:i] + random.choice(alp) + c[i+1:]
+				c = c[:i] + random.choice(alp) + c[i+1:]         # Gene changing due to mutation...
 		return c
 
 
 alp = "abcdefghijklmnopqrstuvwxyz"
-ob = person("unicorn")
-a = 96
-population = {}
-for i in range(0,26):
-	a = a+1
-	#print(chr(a))
-	geno = []
-	pheno = []
-	for j in range(0,7):
-		geno.append(random.choice(alp))
-		pheno.append(random.choice(alp))
 
-	ob.values(chr(a),''.join(geno),''.join(pheno))
-	ob.prepare_population()
-ob.fitness()
-#print(population)
+target = input("Input a target string\n")
+cross = []
+mutate = ""
+ob = Genetic(target)
 
-ob.prepare_pool()
-ob.parent_selection()
 print("Gene_Transform based on Genotype\n")
-print("cross_over result : ",''.join(ob.cross_over()))
-print("mutation_result : ",ob.mutation())
+
+z = 0
+while ''.join(cross) != target and z<20000000:
+	population = {}
+	a = 96
+	for i in range(0,26):
+		a = a+1
+		geno = []
+		pheno = []
+		for j in range(0,len(target)):
+			geno.append(random.choice(alp))
+			pheno.append(random.choice(alp))
+
+		ob.values(chr(a),''.join(geno),''.join(pheno))
+		ob.prepare_population()
+
+
+
+	ob.fitness()
+	ob.prepare_pool()
+	ob.parent_selection()
+	cross = ob.cross_over()
+	mutate = ob.mutation()
+
+	z += 1
+print("Total generations : ",z)
+print("Mutation at %dth generation is  "%(z),mutate)
+
