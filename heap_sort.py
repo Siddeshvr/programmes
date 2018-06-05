e4757f8e86cca441e1157f89d8a77128812d085e
siddesh <siddeshvr1999@gmail.com>
2018-06-05 15:33:32 +0530

    Animated tree

--- heap_sort.py
+++ heap_sort.py
@@ -1,26 +1,26 @@
-import pygame,math,sys
+import pygame,math,sys           #importing files...
 from pygame.locals import *
 
-pygame.init()
+pygame.init()         # Initiating pygame...
 
-screen = pygame.display.set_mode((800,600),0,32)
+screen = pygame.display.set_mode((800,600),0,32)      #creating screen...
 pygame.display.set_caption("Heap Sort")
-font = pygame.font.SysFont('Times new Roman',18)
+font = pygame.font.SysFont('Times new Roman',18)      #font to insert value inside circle...
 
-
+#Module to draw a circle...
 def drawcircle(a,b):
 	pygame.draw.circle(screen,(255,255,255),[a,b],20)
 	
-
+#module to insert a specified value to specified circle...
 def value(i,a,b):
 	x = font.render(str(i) , 1 , (0,0,128))
 	screen.blit(x , (a,b))
 	
-
+#Creating branches between two nodes...
 def drawline(a,b,x,y):
 	pygame.draw.lines(screen,(255,255,255),False,[(a,b),(x,y)],1)
 	
-
+#Module to create full tree...
 def create_tree(L,n):
 	A = [[400,50],[250,200],[550,200],[175,350],[325,350],[475,350],[625,350],[137,500],[213,500],[287,500],[363,500],[437,500]]
 	for i in range(n):
@@ -42,11 +42,11 @@ print("Enter ",str(n)," elements : ")
 for i in range(n):
 	L.append(int(input()))
 create_tree(L,len(L))
-pygame.display.update()
+pygame.display.update()          #Displaying created tree on pygame window...
 print("Exit from Pygame window")
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			pygame.quit(); sys.exit();
+			pygame.quit(); sys.exit(); #To exit from pygame window...
 
