2ddf43b46524faa276873e05383e6c39c149bbef
siddesh <siddeshvr1999@gmail.com>
2018-06-06 00:05:59 +0530

    Animated Heap_sort

--- heap_sort.py
+++ heap_sort.py
@@ -1,11 +1,11 @@
 import pygame,sys           #importing files...
-from pygame.locals import *
 
 pygame.init()         # Initiating pygame...
+clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((800,600),0,32)      #creating screen...
 pygame.display.set_caption("Heap Sort")
-font = pygame.font.SysFont('Times new Roman',18)      #font to insert value inside circle...
+font = pygame.font.SysFont('Times new Roman',30)      #font to insert value inside circle..."Times new Roman" is a font name...
 
 #Module to draw a circle...
 def drawcircle(a,b):
@@ -13,8 +13,8 @@ def drawcircle(a,b):
 	
 #module to insert a specified value to specified circle...
 def value(i,a,b):
-	x = font.render(str(i) , 1 , (0,0,128))
-	screen.blit(x , (a,b))
+	x = font.render(str(i) , 1 , (0,0,128))       #insertin value...
+	screen.blit(x , (a,b))						  #pygame renders text as image...so need to blit...
 	
 #Creating branches between two nodes...
 def drawline(a,b,x,y):
@@ -22,16 +22,16 @@ def drawline(a,b,x,y):
 
 	
 #Module to create full tree...
-def create_tree(L,n):
+def create_tree(L,m):
 	A = [[400,50],[250,200],[550,200],[175,350],[325,350],[475,350],[625,350],[137,500],[213,500],[287,500],[363,500],[437,500]]
-	for i in range(n):
+	for i in range(m):
 		drawcircle(A[i][0] , A[i][1])
-		value(L[i] , A[i][0]-8 , A[i][1]-8)
+		value(L[i] , A[i][0]-12 , A[i][1]-13)
 	k = 1
 	i = 0
-	while i<10 and k < n:
+	while i<10 and k < m:
 		j = 0
-		while j<2 and k<n:
+		while j<2 and k<m:
 			drawline(A[i][0],A[i][1] , A[k][0],A[k][1])
 			k += 1
 			j += 1
@@ -39,32 +39,39 @@ def create_tree(L,n):
 
 
 
-
-def Heapsort(L,n):
-	for i in range(int(n/2-1),-1,-1):
-		max_heapify(L,n,i)
+def Heapsort(L,n,m):
+	for i in range(int(m/2-1),-1,-1):
+		max_heapify(L,m,i)
 
 	for i in range(n-1,-1,-1):
+		screen.fill((0,0,0))
 		L[0],L[i] = L[i],L[0]
-		max_heapify(L,i,0)
-		create_tree(L,n)
+		create_tree(L,m)
 		pygame.display.update()
+		clock.tick(0.5)
+		max_heapify(L,i,0)
+		m = m-1
+		create_tree(L,m)
+		pygame.display.update()
+		clock.tick(0.5)
 
-def max_heapify(L,n,i):
+
+def max_heapify(L,m,i):
 	largest = i
 	l = 2*i +1
 	r = 2*i +2
-	if l<n and L[l]<L[largest]:
+	if l<m and L[l]>L[largest]:
 		largest = l
-	if r<n and L[r]<L[largest]:
+	if r<m and L[r]>L[largest]:
 		largest = r
 
 	if largest != i:
 		L[i],L[largest] = L[largest],L[i]
-		max_heapify(L,n,largest)
-		create_tree(L,n)
+		screen.fill((0,0,0))
+		max_heapify(L,m,largest)
+		create_tree(L,m)
 		pygame.display.update()
-
+		clock.tick(0.5)
 
 
 L = []
@@ -72,13 +79,18 @@ n = int(input("Number of elements\n"))
 print("Enter ",str(n)," elements : ")
 for i in range(n):
 	L.append(int(input()))
+m = len(L)
 create_tree(L,len(L))
 pygame.display.update()
-Heapsort(L,n)
-print("Exit from Pygame window")
+clock.tick(0.5)
+Heapsort(L,n,m)
+screen.fill((0,0,0))
+pygame.display.update()
+print("Press (X) mark in the pygame window to exit")
+
 
 while True:
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
+		if event.type == pygame.QUIT :  #when closing window...
 			pygame.quit(); sys.exit(); #To exit from pygame window...
 
