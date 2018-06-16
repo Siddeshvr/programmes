6e502affc2a8472180a50e85f56c27c482db6b1c
siddesh <siddeshvr1999@gmail.com>
2018-06-16 12:45:02 +0530

    Insertion,Traversal and graph display

--- 2-3_trees1.cpp
+++ 2-3_trees1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream>
+#include <fstream>  //header file for file managing...
 #include <stdlib.h>
 using namespace std;
 
@@ -9,7 +9,7 @@ class Node
     int key[3];    //array of keys...
     Node *child[4]; //array for child...
     bool leaf;
-    Node *parent;
+    Node *parent;  //variable for parent of a node...
     public:
     Node* getNode();
     Node* getNode(int item);
@@ -20,6 +20,7 @@ class Node
     void Graph(Node *root);
 };
 
+//Return empty node...
 Node* Node :: getNode()
 {
     Node *temp = new Node[10];
@@ -29,6 +30,7 @@ Node* Node :: getNode()
     return temp;
 }
 
+//Returns weighted node...
 Node* Node :: getNode(int item)
 {
     Node *temp = new Node[10];
@@ -42,9 +44,10 @@ Node* Node :: getNode(int item)
     return temp;
 }
 
+//to write a file...
 void Node :: write_item(Node *root , std::ofstream& ofile)
 {
-    if(root == NULL)
+    if(root == NULL)   //to exit from recursion if no further nodes...
         return;
     
     if(root->leaf == true and root->parent==NULL)   //only for root node...
@@ -54,10 +57,10 @@ void Node :: write_item(Node *root , std::ofstream& ofile)
             ofile<<root->key[i]<<",";
         ofile<<'"';
     }
-    else if(root->n == 1)
+    else if(root->n == 1)     //node with two childrens...
     {
         int k=0;
-        while(k<2 and root->child[0]!=NULL)
+        while(k<2 and root->child[0]!=NULL)  //To process both childrens...
         {
             ofile<<'"';
             for(int i=0;i<root->n;i++)
@@ -68,10 +71,10 @@ void Node :: write_item(Node *root , std::ofstream& ofile)
             ofile<<'"'<<endl;
             k++;
         }
-        write_item(root->child[0],ofile);
-        write_item(root->child[1],ofile);
+        write_item(root->child[0],ofile);  //left child traversal...
+        write_item(root->child[1],ofile);  //right child traversal...
     }
-    else if(root->child[2]!=NULL)
+    else if(root->child[2]!=NULL)   //node with 3 childrens...
     {
         int k=0;
         while(k<3)
@@ -85,38 +88,40 @@ void Node :: write_item(Node *root , std::ofstream& ofile)
             ofile<<'"'<<endl;
             k++;
         }
-        write_item(root->child[0],ofile);
-        write_item(root->child[1],ofile);
-        write_item(root->child[2],ofile);
+        write_item(root->child[0],ofile);  //1st child traversal...
+        write_item(root->child[1],ofile);  //2nd child traversal...
+        write_item(root->child[2],ofile);  //3rd child traversal...
     }
 }
 
-void Node :: Graph(Node *root)
+//To create Graph using Graphviz...
+void Node :: Graph(Node *root)       
 {
     ofstream ofile;
-    ofile.open("diagram.dot");
+    ofile.open("diagram.dot");    //creating file...
     ofile<<"Graph{\n";
     write_item(root,ofile);
     ofile<<"}\n";
     ofile.close();
 }
 
-void Node :: Traversal(Node *root)
+//Inorder traversal to display elements in sorted order...
+void Node :: Traversal(Node *root)    
 {
-    if(root == NULL)
+    if(root == NULL)        //Base case of recursion...
         return;
 
     if(root->leaf == true)
         for(int i=0;i<root->n;i++)
             cout<<root->key[i]<<" ";
-    else if(root->n == 1)
+    else if(root->n == 1)           //Node containing 2 childs...
     {
         Traversal(root->child[0]);
         for(int i=0;i<root->n;i++)
             cout<<root->key[i]<<" ";
         Traversal(root->child[1]);
     }
-    else
+    else            //Node containing 3 childs...
     {
         Traversal(root->child[0]);
         cout<<root->key[0]<<" ";
@@ -126,6 +131,7 @@ void Node :: Traversal(Node *root)
     }
 }
 
+//To split full node...
 Node* Node :: splitNode(Node *node)
 {
     if(node->n != 3)            //Base case to stop recursion...
@@ -133,7 +139,7 @@ Node* Node :: splitNode(Node *node)
     
     if(node->parent == NULL)   //splitting root node...
     {
-        if(node->child[0] == NULL and node->child[1]==NULL)
+        if(node->child[0] == NULL and node->child[1]==NULL)  //when root has no childs...
         {
             node->child[0] = getNode(node->key[0]);
             node->child[1] = getNode(node->key[2]);
@@ -174,15 +180,15 @@ Node* Node :: splitNode(Node *node)
             return temp;
         }
     }
-    else
+    else   //if node is not root...
     {
-        if(node->child[0] == NULL and node->child[1]==NULL)
+        if(node->child[0] == NULL and node->child[1]==NULL)   //If that node has no childs...
         {
-            Node *temp = getNode();
+            Node *temp = getNode();   //To make temp as nodes parent...
             temp = node->parent;
             int j = temp->n;
 
-            while(j>=0 and temp->child[j] != node)
+            while(j>=0 and temp->child[j] != node)         //To create nodes new sibling...
             {
                 temp->child[j+1] = getNode();
                 temp->child[j+1] = temp->child[j];
@@ -190,12 +196,12 @@ Node* Node :: splitNode(Node *node)
             }
 
             int i = temp->n - 1;
-            while(i>=0 and temp->key[i] > node->key[1])
+            while(i>=0 and temp->key[i] > node->key[1])   //to insert middle value to parent in sorted order...
             {
                 temp->key[i+1] = temp->key[i];
                 i--;
             }
-            temp->key[i+1] = node->key[1];
+            temp->key[i+1] = node->key[1];        //Inserting at correct address...
             temp->n += 1;
             temp->child[j+1] = getNode(node->key[2]);
 
@@ -208,15 +214,15 @@ Node* Node :: splitNode(Node *node)
             temp->child[j]->parent = temp;
             temp->child[j]->leaf = true;
 
-            if(temp->n != 3)           //else split...
-                return temp;      //return root...
+            if(temp->n != 3)           //If node->parent(temp) is not full return...else split by recursion...
+                return temp;      
         }
-        else   
+        else                          //To split node which has childs...
         {
             Node *temp = getNode();
             temp = node->parent;
             int j = temp->n;
-            while(j>=0 and temp->child[j] != node)
+            while(j>=0 and temp->child[j] != node)   //To create nodes new sibling...
             {
                 temp->child[j+1] = getNode();
                 temp->child[j+1] = temp->child[j];
@@ -224,12 +230,12 @@ Node* Node :: splitNode(Node *node)
             }
 
             int i = temp->n - 1;
-            while(i>=0 and temp->key[i] > node->key[1])
+            while(i>=0 and temp->key[i] > node->key[1])     //to insert middle value to parent in sorted order...
             {
                 temp->key[i+1] = temp->key[i];
                 i--;
             }
-            temp->key[i+1] = node->key[1];
+            temp->key[i+1] = node->key[1];         //To insert at correct address...
             temp->n += 1;
 
             Node *Z = getNode();
@@ -252,11 +258,11 @@ Node* Node :: splitNode(Node *node)
             node->n = 1;
             temp->child[j] = node;  
 
-            return temp;
+            return temp;          
         }
     }
-    if(node->parent != NULL)
-        node = splitNode(node->parent);
+    if(node->parent != NULL)           
+        node = splitNode(node->parent);   //To check whether node->parent is full or not...if full split...
     return node;
 }
 
@@ -276,12 +282,12 @@ Node* Node :: insert(Node *root , int item)
         root->key[i+1] = item;
         root->n += 1;
 
-        if(root->n == 3)
+        if(root->n == 3)           //If node is full , split...
             root = splitNode(root);
 
-        while(root->parent != NULL)
+        while(root->parent != NULL)     //To search for root...
             root = root->parent;
-        return root;
+        return root;             //Return root...
     }
     else
     {
@@ -296,7 +302,7 @@ Node* Node :: insert(Node *root , int item)
 int main()
 {
 	Node ob;
-	Node *root;
+	Node *root;  //Initail root which is NULL...
 	root = NULL;
 	int c;
 	while(true)
