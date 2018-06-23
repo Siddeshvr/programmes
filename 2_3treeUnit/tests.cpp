b30274f1db866e1234d01b365e56d911d2099c84
siddesh <siddeshvr1999@gmail.com>
2018-06-23 07:41:03 +0530

    unit test for 2-3 trees

--- 2_3treeUnit/tests.cpp
+++ 2_3treeUnit/tests.cpp
@@ -13,12 +13,92 @@ TEST_F(Insert,root)
 {
     Node ob;
     Node *root = NULL;
-    root = ob.insert(root,10);
+    root = ob.insert(root,5);
     EXPECT_EQ(1,root->n);
-    root = ob.insert(root,30);
+    root = ob.insert(root,2);
     EXPECT_EQ(2,root->n);
-    root = ob.insert(root,50);
+    root = ob.insert(root,6);
     EXPECT_EQ(1,root->n);
+    EXPECT_EQ(1,root->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->n);
+
+    root = ob.insert(root,9);
+    EXPECT_EQ(2,root->child[1]->n);
+
+    root = ob.insert(root,10);
+    EXPECT_EQ(2,root->n);
+    EXPECT_EQ(1,root->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->n);
+    EXPECT_EQ(1,root->child[2]->n);
+
+    root = ob.insert(root,4);
+    EXPECT_EQ(2,root->n);
+    EXPECT_EQ(2,root->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->n);
+    EXPECT_EQ(1,root->child[2]->n);
+
+    root = ob.insert(root,1);
+    EXPECT_EQ(1,root->n);
+    EXPECT_EQ(1,root->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->n);
+    EXPECT_EQ(1,root->child[0]->child[0]->n);
+    EXPECT_EQ(1,root->child[0]->child[1]->n);
+    EXPECT_EQ(1,root->child[1]->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->child[1]->n);
+
+    root = ob.insert(root,11);
+    EXPECT_EQ(1,root->n);
+    EXPECT_EQ(1,root->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->n);
+    EXPECT_EQ(1,root->child[0]->child[0]->n);
+    EXPECT_EQ(1,root->child[0]->child[1]->n);
+    EXPECT_EQ(1,root->child[1]->child[0]->n);
+    EXPECT_EQ(2,root->child[1]->child[1]->n);
+
+    root = ob.insert(root,12);
+    EXPECT_EQ(1,root->n);
+    EXPECT_EQ(1,root->child[0]->n);
+    EXPECT_EQ(2,root->child[1]->n);
+    EXPECT_EQ(1,root->child[0]->child[0]->n);
+    EXPECT_EQ(1,root->child[0]->child[1]->n);
+    EXPECT_EQ(1,root->child[1]->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->child[1]->n);
+    EXPECT_EQ(1,root->child[1]->child[2]->n);
+
+    root = ob.insert(root,13);
+    EXPECT_EQ(1,root->n);
+    EXPECT_EQ(1,root->child[0]->n);
+    EXPECT_EQ(2,root->child[1]->n);
+    EXPECT_EQ(1,root->child[0]->child[0]->n);
+    EXPECT_EQ(1,root->child[0]->child[1]->n);
+    EXPECT_EQ(1,root->child[1]->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->child[1]->n);
+    EXPECT_EQ(2,root->child[1]->child[2]->n);
+
+    root = ob.insert(root,14);
+    EXPECT_EQ(2,root->n);
+    EXPECT_EQ(1,root->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->n);
+    EXPECT_EQ(1,root->child[2]->n);
+    EXPECT_EQ(1,root->child[0]->child[0]->n);
+    EXPECT_EQ(1,root->child[0]->child[1]->n);
+    EXPECT_EQ(1,root->child[1]->child[0]->n);
+    EXPECT_EQ(1,root->child[1]->child[1]->n);
+    EXPECT_EQ(1,root->child[2]->child[0]->n);
+    EXPECT_EQ(1,root->child[2]->child[1]->n);
+
+    EXPECT_EQ(5,root->key[0]);
+    EXPECT_EQ(11,root->key[1]);
+    EXPECT_EQ(2,root->child[0]->key[0]);
+    EXPECT_EQ(9,root->child[1]->key[0]);
+    EXPECT_EQ(13,root->child[2]->key[0]);
+    EXPECT_EQ(1,root->child[0]->child[0]->key[0]);
+    EXPECT_EQ(4,root->child[0]->child[1]->key[0]);
+    EXPECT_EQ(6,root->child[1]->child[0]->key[0]);
+    EXPECT_EQ(10,root->child[1]->child[1]->key[0]);
+    EXPECT_EQ(12,root->child[2]->child[0]->key[0]);
+    EXPECT_EQ(14,root->child[2]->child[1]->key[0]);
+
     
     /*
     int expected[] = {10,30,50};
