b6d20388f5ae5301c84f69a374381d26c1740a9d
siddesh <siddeshvr1999@gmail.com>
2018-06-27 22:45:32 +0530

    Working sheet

---
+++ java/learn.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class learn
+{
+    /*
+    //public static void Sorting(int x[],int n)  //If no object,then use this...
+    void Sorting(int x[],int n)
+    {
+        for(int i=0;i<n-1;i++)
+            for(int j=0;j<n-i-1;j++)
+                if(x[j]>x[j+1])
+                {
+                        int temp = x[j];
+                        x[j] = x[j+1];
+                        x[j+1] = temp;
+                }
+    }
+
+    public static void main(String[] args)
+    {
+        learn ob = new learn();  //to create object to call non-static method...
+        int x[] = new int[10];
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter n value : ");
+        int n = s.nextInt();
+        System.out.println("Enter values : ");
+        for(int i=0;i<n;i++)
+            x[i] = s.nextInt();
+        ob.Sorting(x,n);
+        //System.out.println(args[0]+" "+args[1]);
+        for(int i=0;i<n;i++)
+            System.out.println(x[i]);
+    }
+    */
+};
